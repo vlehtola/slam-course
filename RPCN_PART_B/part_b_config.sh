04da4a283b17a9bb8555224ba2277b922e12bb78
Ville Lehtola <v.v.lehtola@utwente.nl>
2024-08-09 15:29:24 +0200

    remove apt install rviz

--- RPCN_PART_B/part_b_config.sh
+++ RPCN_PART_B/part_b_config.sh
@@ -4,13 +4,13 @@ source "/opt/cartographer_ros/setup.bash"
 
 mkdir -p /home/rpcn/catkin_ws/src
 cp -rf /rpcn_part_b /home/rpcn/catkin_ws/src
-cp -rf /bagfiles /home/rpcn/catkin_ws/src/rpcn_part_b/
 cd /home/rpcn/catkin_ws
 catkin_make
-rm -rf /rpcn_part_b /bagfiles
+rm -rf /rpcn_part_b
 sudo apt-get update --fix-missing
-apt-get -y install dialog apt-utils vim sudo wget usbutils nano curl git vim-gtk3 rviz
+apt-get -y install vim nano
+# apt-get -y install dialog apt-utils vim sudo wget usbutils nano curl git vim-gtk3 rviz
 
-apt-get install ros-noetic-rviz
+# apt-get install ros-noetic-rviz
 
 
